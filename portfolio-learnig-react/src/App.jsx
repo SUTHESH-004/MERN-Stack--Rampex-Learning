9f97029a28a93750c3e88cdf7023c6f9fc51a682
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-09-18 10:15:52 +0530

    creating the component nav bar

--- portfolio-learnig-react/src/App.jsx
+++ portfolio-learnig-react/src/App.jsx
@@ -5,12 +5,14 @@ function App() {
   return (
     <>
       <div
-        className="fixed z-0 h-[100vh] w-[100vw]"
+        className="fixed z-1 top-0 h-[100vh] w-[100vw]"
         style={{
           backgroundImage: `url(${assets.staticWallpaper})`,
         }}
-      ></div>
-      <div className="fixed z-0 h-[100vh] w-[100vw] bg-black">
+      >
+        {" "}
+      </div>
+      <div className="fixed z-0 h-[100vh] w-[100vw]">
         <Navbar />
       </div>
     </>
--- portfolio-learnig-react/src/components/Navbar.jsx
+++ portfolio-learnig-react/src/components/Navbar.jsx
@@ -2,12 +2,15 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="h-[100px] w-[100vw] bg-blue-500 rounded-2xl border-x-gray-950">
-        <div className="h-[90px] w-[100px] bg-green-300">
-            <div></div>
-            <div></div>
-        </div>
+    <div className="h-[100px] w-[90vw] bg-blue-500 rounded-2xl border-x-gray-950 m-2 mx-auto flex justify-between items-center">
+      <div className="h-[80px] w-[600px] bg-green-300 my-1 mx-3 rounded-2xl">
         <div></div>
+        <div></div>
+      </div>
+      <div className="h-[80px] w-[300px] bg-green-300 my-1 mx-3 rounded-2xl">
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 };
